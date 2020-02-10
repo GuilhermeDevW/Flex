6a906cb1e8cd89e04b4f9fb6c3ea6021b8f95903
GuilhermeDevW <guilherme.carvalgoG@Outlook.com>
2020-02-10 02:39:51 -0300

    Removendo Bug

--- Controller/homeController.php
+++ Controller/homeController.php
@@ -1,8 +1,9 @@
 <?php
-include_once "abstarctController.php";
+
 class homeController {
 
-    public function add(){
+    public function add()
+    {
         $URL_ATUAL= "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
         if($URL_ATUAL=="http://localhost/Flex/" || $URL_ATUAL=="http://localhost/flex/index.php" || $URL_ATUAL=="http://localhost/flex/"){
             include_once "Model/banco.php";
@@ -15,27 +16,33 @@ class homeController {
   
     }
     
-    public function getIndex(){
+    public function getIndex()
+    {
          return include "View/home.php";
     }
 
-    public function getTabela(){
+    public function getTabela()
+    {
       return  $this->add()->getContato();
      }
 
-    public function addContato($t,$n){
+    public function addContato($t,$n)
+    {
         return  $this->add()->insertContato($t,$n);
     }
 
-    public function deleteContato($id){
+    public function deleteContato($id)
+    {
         return  $this->add()->deleteContato($id);
     }
 
-    public function getU($i){
+    public function getU($i)
+    {
         return  $this->add()->getU($i);
     }
 
-    public function updateContato($id,$numero,$nome){
+    public function updateContato($id,$numero,$nome)
+    {
        
         return  $this->add()->updateContato($id,$numero,$nome);
     }
